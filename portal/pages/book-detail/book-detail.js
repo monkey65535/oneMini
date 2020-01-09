17cbac67cb23acf1d3853df6bfed685ee91bd7f8
admin <test@163.com>
2020-01-09 16:49:52 +0800

    小程序样式完成

--- portal/pages/book-detail/book-detail.js
+++ portal/pages/book-detail/book-detail.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isTrigger: false
   },
 
   /**
@@ -62,5 +62,11 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  handleTriggerTags(event) {
+    const isT = this.data.isTrigger
+    this.setData({
+      isTrigger: !isT
+    })
   }
 })
