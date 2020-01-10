e216639a778aa27ed4299582ddd36c03af6a3e86
admin <test@163.com>
2020-01-10 17:26:27 +0800

    app init

--- server/app/api/classic.js
+++ server/app/api/classic.js
@@ -4,6 +4,7 @@ const router = new Router({
 });
 router.get('/', async (ctx, next) => {
     ctx.body = 'HELLO MINI PRO'
+    // todo  访问接口 返回当前页信息和count, 如果携带要访问的id,那么返回对应id的信息和count
 })
 
 module.exports = router;
